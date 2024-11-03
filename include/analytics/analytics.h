b203569cb6af6920d639818903ea88b6881456fa
Arianna <143475620+arianna-k@users.noreply.github.com>
2024-11-03 15:47:50 -0500

    Header Files for Analytic Classes

---
+++ include/analytics/analytics.h
@@ -0,0 +1,17 @@
+#ifndef ANALYTICS_H
+#define ANALYTICS_H
+
+#include <map>
+#include "Robot.h"
+#include "RobotMetrics/robot_metrics.h"
+
+class Analytics {
+private:
+    std::map<Robot, RobotMetrics> robotData;
+
+public:
+    void generateReport();
+    float getRobotEfficiency(const Robot& robot);
+};
+
+#endif // ANALYTICS_H
