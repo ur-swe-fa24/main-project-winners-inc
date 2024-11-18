9ce9a8eef5173d18b3fea2fe467f42893d0239a8
Charlotte Brand <charlotte.brand@richmond.edu>
2024-11-18 12:26:44 -0500

    Initial implementation based on class diag

---
+++ include/Schedule/schedule.h
@@ -0,0 +1,20 @@
+#ifndef SCHEDULE_H
+#define SCHEDULE_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <memory>
+#include "cleaningTask/cleaningTask.h"
+
+
+class Schedule {
+public:
+    // Vector of tasks to maintain an ordered list of tasks
+    std::vector<std::shared_ptr<CleaningTask>> tasks;
+
+    void addTaskToSchedule(const std::shared_ptr<CleaningTask>& task) {}
+    void removeTaskFromSchedule() {}
+};
+
+#endif // SCHEDULE_H
