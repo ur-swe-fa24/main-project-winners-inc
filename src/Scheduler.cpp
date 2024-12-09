02d349f24ad17e76e3525c5feaff0b724b7f5819
Charlotte Brand <charlotte.brand@richmond.edu>
2024-12-08 21:01:12 -0500

    More alert implementation

--- src/Scheduler.cpp
+++ src/Scheduler.cpp
@@ -5,6 +5,7 @@
 #include "AlertSystem/alert_system.h"
 #include "adapter/MongoDBAdapter.hpp"
 #include "alert/Alert.h"
+#include "AlertDialog/AlertDialog.hpp"
 #include <algorithm>
 #include <stdexcept>
 #include <ctime>
@@ -124,34 +125,36 @@ void Scheduler::checkAndReturnToChargerIfNeeded(std::shared_ptr<Robot> robot) {
     if (needsReturn && simulator_) {
         std::cout << "[DEBUG] " << robot->getName() << " returning to charger.\n";
         simulator_->requestReturnToCharger(robot->getName());
-        
-        // if (alertSystem_ && (battery < 20.0)) {
-        //     alertSystem_->sendAlert(name + " has low battery levels. Returning to charger.\n", "Battery");
-        // }
-        // if (dbAdapter_ && (battery < 20.0)) {
-        //     Alert newAlert("Battery",
-        //                 name + " has low battery levels. Returning to charger.",
-        //                 robot,
-        //                 std::make_shared<Room>(*errorRoom),
-        //                 std::time(nullptr),
-        //                 Alert::LOW);
-        //     dbAdapter_->saveAlert(newAlert);
-        // }
 
+        if (alertSystem_ && (battery < 20.0)){
+            std::string message = name + " has low battery levels. Returning to charger.";
+            alertSystem_->sendAlert(message, "Task");
+        }
 
-        // if (alertSystem_ && (water < 20.0)) {
-        //     alertSystem_->sendAlert(name + " has low water levels. Refill tank.", "Water");
-        // }
-        // if (dbAdapter_ && (water < 20.0)) {
-        //     Alert newAlert("Water",
-        //                 name + " has low water levels. Refill tank.",
-        //                 robot,
-        //                 std::make_shared<Room>(errorRoom),
-        //                 std::time(nullptr),
-        //                 Alert::LOW);
-        //     dbAdapter_->saveAlert(newAlert);
-        // }
+        if (alertSystem_ && (water < 20.0)) {
+            std::string message = name + " has low water levels. Refill tank.";
+            alertSystem_->sendAlert(message, "Task");
+        }
 
+        if (dbAdapter_ && (battery < 20.0)) {
+            Alert newAlert("Task",
+                           name + " has low battery levels. Returning to charger.",
+                           robot,
+                           std::make_shared<Room>(*errorRoom),
+                           std::time(nullptr),
+                           Alert::LOW);
+            dbAdapter_->saveAlert(newAlert);
+        }
+
+        if (dbAdapter_ && (water < 20.0)) {
+            Alert newAlert("Task",
+                           name + " has low water levels. Refill tank.",
+                           robot,
+                           std::make_shared<Room>(*errorRoom),
+                           std::time(nullptr),
+                           Alert::LOW);
+            dbAdapter_->saveAlert(newAlert);
+        }
     }
 }
 
